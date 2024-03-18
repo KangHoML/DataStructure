acac3433998ec662403b6a308797fd42b3ecd40a
KangHoML <kanghokhkh1@gmail.com>
2024-03-18 12:15:06 +0900

    w3_monday

---
+++ Week3/complex.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct complex {
+    float re;
+    float im;
+}complex;
+
+complex multiply(complex a, complex b) {
+    complex c;
+    c.re = a.re * b.re - a.im * b.im;
+    c.im = a.re * b.im + a.im * b.re;
+    return c;
+}
+
+void main() {
+    complex a = {1.3, 3.9};
+    complex b = {-3.3, 8.4};
+    complex result;
+
+    result = multiply(a, b);
+    printf("re: %f, im: %f\n", result.re, result.im);
+}
---
+++ Week3/memory_alloc.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warninig(disable: 4996)
+
+void compute(int** matrix, int len);
+void main() {
+    int len;
+    printf("input matrix size: ");
+    scanf("%d", &len);
+    int** matrix = (int**)calloc(len, sizeof(int*));
+    for(int i = 0; i < len; i++) {
+        *(matrix + i) = (int *)calloc(len, sizeof(int));
+    }
+
+    compute(matrix, len);
+    for(int i = 0; i < len; i++) {
+        free(*(matrix + i));
+    }
+    free(matrix);
+}
+
+void compute(int** matrix, int len) {
+    float avg = 0.0;
+    for(int i = 0; i < len; i++) {
+        for (int j = 0; j < len; j++) {
+            scanf("%d", (*(matrix + i) + j));
+            avg +=(float)(*(*(matrix + i) + j));
+        }
+    }
+    avg /= (float)(len * len);
+    printf("%f\n", avg);
+}
