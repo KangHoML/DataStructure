9f8b08dfff3db5c1f8fb0dde310a99042937a51c
KangHoML <kanghokhkh1@gmail.com>
2024-04-16 00:02:00 +0900

    hw1 comment

--- Week7/connected_component.c
+++ Week7/connected_component.c
@@ -25,7 +25,7 @@ int dy[8] = { 0, 1, 1, 1, 0, -1, -1, -1 };
 
 // stack을 위한 linked list 구조체 정의
 typedef struct _node {
-    int data[3];
+    int data[3]; // 데이터를 직접적으로 저장하기 위해 배열로 저장
     struct _node* next;
 }node;
 
@@ -41,14 +41,17 @@ void labeling(int x, int y);
 
 // main 함수
 void main() {
+    // stack 초기화
     init_stack();
 
+    // 전체 배열에 대해 완전 탐색
     for (int i = 0; i < 15; i++) {
         for (int j = 0; j < 15; j++) {
             labeling(i, j);
         }
     }
 
+    // 출력
     for (int i = 0; i < 15; i++) {
         for (int j = 0; j < 15; j++) {
             printf("%-3d", input_map[i][j]);
@@ -87,7 +90,7 @@ int push(int* in_data) {
 // stack에 저장된 [x, y, dir] 정보를 꺼내 반환
 int* pop() {
     node* temp;
-    int* out_data = (int *)calloc(3, sizeof(int));
+    int* out_data = (int *)calloc(3, sizeof(int)); // pop()을 통해 출력할 데이터를 동적할당
 
     if (head->next == tail)
         return NULL;
@@ -98,7 +101,8 @@ int* pop() {
     out_data[2] = temp->data[2];
 
     head->next = temp->next;
-    free(temp);
+    free(temp); // 비워주기
+
     return out_data;
 }
 
@@ -106,35 +110,40 @@ int* pop() {
 void labeling(int r, int c) {
     int x, y, d, nx, ny;
     int* in_data, * out_data;
-    int flag = 0;
+    int flag = 0; // 한번이라도 탐색이 되었는가를 확인하기 위한 flag
 
     // 탐색할 인덱스에 대한 정보 push
     in_data = (int*)calloc(3, sizeof(int));
     in_data[0] = r, in_data[1] = c, in_data[2] = 0;
     push(in_data);
 
-    // stack이 비었을 때까지
     while (1) {
+        // stack이 비었을 때까지 반복
         if ((out_data = pop()) == NULL)
             break;
         x = out_data[0], y = out_data[1], d = out_data[2];
 
+        // 8 개의 방향을 탐색
         for (int i = 0; i < 8; i++) {
             nx = x + dx[(d + i) % 8];
             ny = y + dy[(d + i) % 8];
-
+            
+            // 범위 안에 존재하며 라벨링이 필요한 부분인지 체크
             if (nx >= 0 && nx < 15 && ny >= 0 && ny < 15 && input_map[nx][ny] == 1) {
-                input_map[nx][ny] = label;
+                input_map[nx][ny] = label; // 현재 라벨 값을 넣어줌
                 in_data[0] = nx, in_data[1] = ny, in_data[2] = (d + i) % 8;
-                push(in_data);
+                push(in_data); // 조건에 만족하는 부분 stack에 push (다음에 탐색하기 위해)
                 flag = 1;
             }
         }
     }
 
+    // 라벨링이 된 경우에만 label 증가
     if (flag == 1) {
         label++;
     }
+
+    // 동적할당한 메모리 해제
     free(in_data);
     free(out_data);
 }
