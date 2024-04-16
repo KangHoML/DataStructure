9340ad88dac45594fc300dd98f77169b125778a1
KangHoML <kanghokhkh1@gmail.com>
2024-04-16 23:24:33 +0900

    update

--- Week7/connected_component.c
+++ Week7/connected_component.c
@@ -37,20 +37,42 @@ int label = 2;
 void init_stack();
 int push(int* in_data);
 int* pop();
-void labeling(int x, int y);
+void labeling(int x, int y, int d);
 
 // main 함수
 void main() {
+    // 변수 정의
+    int nx, ny, d, flag;
+    int *in_data = (int*)calloc(3, sizeof(int));
+    
     // stack 초기화
     init_stack();
 
     // 전체 배열에 대해 완전 탐색
     for (int i = 0; i < 15; i++) {
         for (int j = 0; j < 15; j++) {
-            labeling(i, j);
+            // 8 개의 방향을 탐색
+            for (int k = 0; k < 8; k++) {
+                nx = i + dx[k];
+                ny = j + dy[k];
+                
+                // 범위 안에 존재하며 라벨링이 필요한 부분인지 체크
+                if (nx >= 0 && nx < 15 && ny >= 0 && ny < 15 && input_map[nx][ny] == 1) {
+                    // 현재 인덱스 push
+                    in_data[0] = i, in_data[1] = j, in_data[2] = k;
+                    push(in_data);
+
+                    // 라벨링
+                    labeling(nx, ny, k);
+                    label++;
+
+                    // 마지막 (i, j) 부분 pop() 해주기
+                    pop();
+                }
+            }
         }
     }
-
+    
     // 출력
     for (int i = 0; i < 15; i++) {
         for (int j = 0; j < 15; j++) {
@@ -59,6 +81,8 @@ void main() {
         printf("\n");
     }
 
+    // 메모리 할당 해제
+    free(in_data);
 }
 
 // stack 초기화
@@ -107,21 +131,24 @@ int* pop() {
 }
 
 // 각 구역 라벨링
-void labeling(int r, int c) {
+void labeling(int r, int c, int dir) {
     int x, y, d, nx, ny;
-    int* in_data, * out_data;
-    int flag = 0; // 한번이라도 탐색이 되었는가를 확인하기 위한 flag
+    int *in_data, *out_data;
+    node *cursor = head->next;
 
-    // 탐색할 인덱스에 대한 정보 push
+    // 초기 인덱스에 대한 정보 push
     in_data = (int*)calloc(3, sizeof(int));
-    in_data[0] = r, in_data[1] = c, in_data[2] = 0;
+    in_data[0] = r, in_data[1] = c, in_data[2] = dir;
     push(in_data);
 
     while (1) {
-        // stack이 비었을 때까지 반복
-        if ((out_data = pop()) == NULL)
+        // stack이 하나 남았을 때까지
+        if (head->next->next == tail)
             break;
+
+        out_data = pop();
         x = out_data[0], y = out_data[1], d = out_data[2];
+        free(out_data);
 
         // 8 개의 방향을 탐색
         for (int i = 0; i < 8; i++) {
@@ -133,17 +160,10 @@ void labeling(int r, int c) {
                 input_map[nx][ny] = label; // 현재 라벨 값을 넣어줌
                 in_data[0] = nx, in_data[1] = ny, in_data[2] = (d + i) % 8;
                 push(in_data); // 조건에 만족하는 부분 stack에 push (다음에 탐색하기 위해)
-                flag = 1;
             }
         }
     }
 
-    // 라벨링이 된 경우에만 label 증가
-    if (flag == 1) {
-        label++;
-    }
-
     // 동적할당한 메모리 해제
     free(in_data);
-    free(out_data);
 }
